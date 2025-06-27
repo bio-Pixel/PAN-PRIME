2830fbe6d52be2a677585fc54d06e471667ec2c8
bio-Pixel <151147853+bio-Pixel@users.noreply.github.com>
2025-06-27 14:30:57 +0800

    Create color.R

---
+++ color.R
@@ -0,0 +1,34 @@
+col_l1 <- c(
+    Tuft = "#E64B35FF", `KRT19+` = "#4DBBD5FF", Ductal = "#00A087FF", ADM = "#3C5488FF", Acinar = "#F39B7FFF",
+    Epi = "#4a6fe3", Proliferating = "#b5bbe3", Treg = "#bec1d4", `CD4+ T` = "#d6bcc0", `CD8+ T` = "#bb7784",
+    NK = "#8dd593", NKT = "#f0b98d", ILC = "#f6c4e1", B = "#023fa5", `Tn` = "#7D58B9",
+    PC = "#11c638", Monocyte = "#F0E442", DC = "#ef9708", Mast = "#ead3c6", Macrophage = "#FEC260",
+    Endocrine = "#8e063b", EC = "#d33f6a", MDSC = "#e6afb9", Schwann = "#0d6c0d", Stellate = "#0fcfc0",
+    Fibroblast = "#7382BC", SMC = "#e07b91", Adipocyte = "#F7C394", Neutrophil = "#ade87c"
+)
+
+col_lym_l2 <- c(
+  "Tn" = "#ED645A", "CRTAM+ gamma_delta T" = "#5D69B1", "CD8+ GAMK+ Tem" = "#DAA51B", "CD8+ MAIT" = "#F28E2B", "CD8+ TRM" = "#FF9D9A",
+  "CD4+ Th" = "#86c7b4", "Treg" = "#8CD17D", "CD4+ Tfh" = "#9cd2ed", "CD8+ CXCL13+ Tex" = "#d4de9c", "CD8+ CD16+ T" = "#B07AA1",
+  "ILC" = "#FABFD2", "CD56+ NK" = "#B6992D", "NKT" = "#499894", "CD16+ TaNK" = "#FFBE7D", "CD16+ NK" = "#E58606",
+  "Bn" = "#CC95C0", "Bm" = "#7F7FD5", "IgG+ PC" = "#4AC29A", "early PC" = "#A0CBE8", "Breg" = "#9D7660",
+  "Plasmablast" = "#134E5E", "IgA+ PC" = "#a8e063", "GC-B" = "#f3f9a7"
+)
+
+col_mye_l2 <- c(
+  `SPP1+ TAM` = "#cbdaa9", `COL1A1+ TAM` = "#df5734", `CD16- CD14+ Mono` = "#d69971", `CD163L1+ TAM` = "#b7deea", `CD16+ CD14+ Mono` = "#d8a0c0",
+  `CD16+ CD14- Mono` = "#bc9a7f", `FOLR2+ TAM` = "#a44e89", MDSC = "#9f8d89", `SLCO2B1+ DCreg` = "#927c9a", pDC = "#c4612f",
+  cDC1 = "#c4daec", `CD207+ DC` = "#63a3b8", cDC2 = "#ebb1a4", `LAMP3+ DC3` = "#61bada", `KDM6B+ Mast` = "#76a2be",
+  `KDM6B- Mast` = "#e6b884", `SPP1+ Neu` = "#64a776", `SELL+ Neu` = "#de8b36"
+)
+
+col_stroma_l2 <- c(
+  LEC = "#b95055", ArtEC = "#cbdaa9", VenEC = "#ebb1a4", Tip = "#405993", NCapEC = "#b85292",
+  EndoMT = "#bc9a7f", Stellate = "#8d689d", SMC = "#a9c2cb", NF = "#da6f6d", `PI16+ CAF` = "#d69a55",
+  myCAF = "#83ab8e", iCAF = "#d8a0c0", apCAF = "#ece399", `APOD+ CAF` = "#e0cfda", Adipocyte = "#d5bb72",
+  Schwann = "#64a776", `NGFR+ Sch` = "#E64B35FF", `MPZ+ Sch` = "#4DBBD5FF", `ZEB1+ Sch` = "#00A087FF"
+)
+
+col_tme = c(TME1 = "#cc6666", TME2 = "#6699cc", TME3 = "#008280", TME4 = "#bb0021", TME5 = "#631879")
+
+
