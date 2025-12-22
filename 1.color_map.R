51749604955e8b13d74c22d54b51fdbee6d8d255
bio-Pixel <151147853+bio-Pixel@users.noreply.github.com>
2025-12-22 11:59:44 +0800

    Update and rename color_map.R to 1.color_map.R

---
+++ 1.color_map.R
@@ -0,0 +1,33 @@
+# Color palettes used across figures in the manuscript
+
+cell_colors <- c(
+    
+  TME1="#CC6666", TME2="#6699CC", TME3="#008280", TME4="#BB0021", TME5="#631879",
+    
+  Tuft="#E64B35FF", `KRT19+`="#4DBBD5FF", Ductal="#00A087FF", ADM="#3C5488FF", Acinar="#F39B7FFF", Exocrine="#4A6FE3", Proliferating="#B5BBE3",
+    
+  `CD4+ T`="#D6BCC0", `CD8+ T`="#BB7784", Tn="#ED645A", Treg="#8CD17D", `CD4+ Th`="#86C7B4", `CD4+ Tfh`="#9CD2ED",
+    
+  `CD8+ Teff`="#B07AA1", `CD8+ TRM`="#FF9D9A", `CD8+ GZMK+ Tem`="#DAA51B", `CD8+ MAIT`="#F28E2B", `CD8+ CXCL13+ T`="#D4DE9C",
+    
+  NK="#8DD593", `CD56+ NK`="#B6992D", `CD16+ NK`="#E58606", NKT="#499894", TaNK="#FFBE7D", ILC="#FABFD2",
+    
+  B="#023FA5", Bn="#CC95C0", Bm="#7F7FD5", `GC-B`="#F3F9A7", Breg="#9D7660", `Trans.B`="#A0CBE8", Plasmablast="#134E5E", `IgG+ PC`="#4AC29A", `IgA+ PC`="#A8E063",
+    
+  Monocyte="#F0E442", Macrophage="#FEC260", DC="#EF9708", Mast="#EAD3C6", Neutrophil="#ADE87C", MDSC="#9F8D89",
+    
+  `SPP1+ TAM`="#CBDAA9", `COL1A1+ TAM`="#DF5734", `FOLR2+ TAM`="#A44E89", `CD163L1+ TAM`="#B7DEEA",
+    
+  cDC1="#C4DAEC", cDC2="#EBB1A4", pDC="#C4612F", `LAMP3+ DC3`="#61BADA",
+    
+  Fibroblast="#7382BC", Stellate="#0FCFC0", PSC="#8D689D", myCAF="#83AB8E", iCAF="#D8A0C0", apCAF="#ECE399",
+    
+  EC="#D33F6A", LEC="#B95055", ArtEC="#CBDAA9", VenEC="#EBB1A4", CapEC="#B85292", Tip="#405993",
+    
+  Schwann="#64A776", `NGFR+ Sch`="#E64B35FF", `MPZ+ Sch`="#4DBBD5FF", `ZEB1+ Sch`="#00A087FF",
+    
+  Alpha="#86C7B4", Beta="#9CD2ED", Gamma="#A992C0", Delta="#EA9994",
+    
+  K1="#007B7F", K2="#DF75AE", K3="#007ABA", K4="#A8C7E9", K5="#00B7CA", K6="#F37121", K7="#8CCA7C", K8="#8B58A4"
+    
+)
